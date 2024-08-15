298d6ba8b4a79f5ae6bc33bc013d7896d1811c4f
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-15 20:08:50 +0700

    create pages client

--- server/index.js
+++ server/index.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 mongoose
