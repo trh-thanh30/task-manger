6c5c9789b515fc9903ed64b621540459105b61a4
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-17 22:21:45 +0700

    create side bar ui

--- client/src/components/SidebarDash.jsx
+++ client/src/components/SidebarDash.jsx
@@ -1,46 +1,76 @@
 "use client";
 
 import { Sidebar } from "flowbite-react";
-import {
-  HiArrowSmRight,
-  HiChartPie,
-  HiInbox,
-  HiShoppingBag,
-  HiTable,
-  HiUser,
-  HiViewBoards,
-} from "react-icons/hi";
-
+import { useEffect, useState } from "react";
+import { HiChartPie } from "react-icons/hi";
+import { MdTask } from "react-icons/md";
+import { CgDanger } from "react-icons/cg";
+import { GiPin } from "react-icons/gi";
+import { GrTask } from "react-icons/gr";
+import { Link, useLocation } from "react-router-dom";
+import { GoSignOut } from "react-icons/go";
 export default function SidebarDash() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [location.search]);
+
   return (
-    <Sidebar aria-label="Default sidebar example">
+    <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
-            Dashboard
-          </Sidebar.Item>
-          <Sidebar.Item
-            href="#"
-            icon={HiViewBoards}
-            label="Pro"
-            labelColor="dark"
-          >
-            Kanban
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiInbox} label="3">
-            Inbox
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiUser}>
-            Users
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiShoppingBag}>
-            Products
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiArrowSmRight}>
-            Sign In
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiTable}>
-            Sign Up
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
+          <Link to={"/dashboard?tab=dash"}>
+            <Sidebar.Item
+              as="div"
+              icon={HiChartPie}
+              active={tab === "dash" || !tab}
+            >
+              Dashboard
+            </Sidebar.Item>
+          </Link>
+
+          <Link to={"/dashboard?tab=my-taks"}>
+            <Sidebar.Item active={tab === "my-taks"} icon={MdTask} as="div">
+              My Taks
+            </Sidebar.Item>
+          </Link>
+
+          <Link to={"/dashboard?tab=vital-task"}>
+            <Sidebar.Item
+              as="div"
+              active={tab === "vital-task"}
+              icon={CgDanger}
+            >
+              Vital Task
+            </Sidebar.Item>
+          </Link>
+
+          <Link to={"/dashboard?tab=task-pin"}>
+            <Sidebar.Item as="div" active={tab === "task-pin"} icon={GiPin}>
+              {" "}
+              Task Pin
+            </Sidebar.Item>
+          </Link>
+
+          <Link to={"/dashboard?tab=task-categories"}>
+            <Sidebar.Item
+              as="div"
+              active={tab === "task-categories"}
+              icon={GrTask}
+            >
+              {" "}
+              Task Categories
+            </Sidebar.Item>
+          </Link>
+
+          <Sidebar.Item icon={GoSignOut} className="cursor-pointer">
+            Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,35 @@
+import { useLocation } from "react-router-dom";
 import FooterCom from "../components/Footer";
 import Header from "../components/Header";
+import SidebarDash from "../components/SidebarDash";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
+  const loaction = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(loaction.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [loaction.search]);
   return (
     <>
       <Header></Header>
-      <div></div>
+      <div className="flex flex-col min-h-screen md:flex-row">
+        <div className="md:w-56">
+          {/* SideBar */}
+          <SidebarDash></SidebarDash>
+        </div>
+
+        {/* Content */}
+        {tab === "dash" && <div className="flex-1 p-4">Content</div>}
+        {tab === "my-taks" && <div className="flex-1 p-4">Content</div>}
+        {tab === "vital-task" && <div className="flex-1 p-4">Content</div>}
+        {tab === "task-pin" && <div className="flex-1 p-4">Content</div>}
+        {tab === "task-categories" && <div className="flex-1 p-4">Content</div>}
+      </div>
       <FooterCom></FooterCom>
     </>
   );
